02e50b03f48c9b9989c6385c9417dfd21cc9fa64
Andreas Boltres <boltres@ais.uni-bonn.de>
2021-10-20 14:05:50 +0200

    moved config params to argparser

--- run.py
+++ run.py
@@ -11,14 +11,6 @@ from ycb_dynamic.output import Writer
 # ==============================================================================
 
 
-SIM_STEPS_PER_SECOND = 1500  # synpick: 500 (might not be enough)
-SIM_STEPS_PER_FRAME = 25
-FPS = SIM_STEPS_PER_SECOND / SIM_STEPS_PER_FRAME  # TODO 60 or even 120 for dynamic movement?
-
-NUM_FRAMES = 180
-SIM_STEPS_PER_EP = SIM_STEPS_PER_FRAME * NUM_FRAMES
-RESOLUTION = (1920, 1080)
-OUT_PATH = Path("output")
 SCENARIOS = { "table": setup_table_scene }
 
 
@@ -43,7 +35,7 @@ def main(cfg):
     scenario_ids = SCENARIOS.keys() if cfg.scenario == "all" else [cfg.scenario]
     for it in range(cfg.iterations):
         for scenario_id in scenario_ids:
-            scene = sl.Scene(RESOLUTION)  # (re-)initialize scene
+            scene = sl.Scene(cfg.resolution)  # (re-)initialize scene
             scenario = SCENARIOS[scenario_id](cfg, scene)  # populate scene according to scenario
             run_and_render_scene(cfg, renderer, scenario, it)
 
@@ -53,25 +45,25 @@ def main(cfg):
 def run_and_render_scene(cfg, renderer, scenario, it):
     '''
     TODO Doc
-    :param cfg:
-    :param scene:
-    :return:
     '''
+
+    print(f"iteration {it}, scenario '{scenario.name}': "
+          f"executing {cfg.sim_steps_per_ep} sim steps and generating {cfg.frames} frames")
 
     scene = scenario.scene
     scene.load_physics()
-
     cameras = scenario.cameras
     writers = [Writer(Path(cfg.out_path) / f"{it:06}_{scenario.name}_{cam.name}") for cam in cameras]
+
     with ExitStack() as stack:
 
         for writer in writers:
             stack.enter_context(writer)
 
-        for t in tqdm.tqdm(range(SIM_STEPS_PER_EP), postfix=f"{SIM_STEPS_PER_EP} sim steps for {NUM_FRAMES} frames"):
+        for t in tqdm.tqdm(range(cfg.sim_steps_per_ep)):
 
             # save visualizations every SIM_STEPS_PER_FRAME sim steps
-            if t % SIM_STEPS_PER_FRAME == 0:
+            if t % cfg.sim_steps_per_frame == 0:
                 for writer, cam in zip(writers, cameras):
                     scene.set_camera_look_at(position=cam.pos, look_at=cam.lookat)
                     result = renderer.render(scene)
@@ -79,11 +71,11 @@ def run_and_render_scene(cfg, renderer, scenario, it):
                     cam.step()  # advance camera for next step if it's a moving one
 
             # sim step
-            scene.simulate(1.0 / SIM_STEPS_PER_SECOND)
+            scene.simulate(1.0 / cfg.sim_steps_per_sec)
 
         if cfg.assemble_rgb:
             for writer in writers:
-                writer.assemble_rgb_video(in_fps=FPS, out_fps=FPS)
+                writer.assemble_rgb_video(in_fps=cfg.sim_fps, out_fps=cfg.sim_fps)
 
 
 # ==============================================================================
@@ -103,15 +95,28 @@ if __name__ == "__main__":
                         help="specify which scenario to create and render")
     parser.add_argument("--assemble-rgb", action="store_true",
                         help="if specified, creates mp4 video files from the RGB frames of an episode")
+    parser.add_argument("--resolution", default=(1920, 1080))
+    parser.add_argument("--frames", type=int, default=180,
+                        help="number of frames generated per episode")
+    parser.add_argument("--sim-steps-per-sec", type=int, default=1500,
+                        help="each simulation step uses the reciprocal of this value as input value for dt")  # synpick: 500 (might not be enough)
+    parser.add_argument("--sim-steps-per-frame", type=int, default=25,
+                        help="number of sim steps passing between each frame")
 
     cfg = parser.parse_args()
+
+    # config preparation
     if cfg.iterations < 1:
         print("parameter 'iterations' < 1, exiting...")
         exit(0)
     else:
         print(f"will generate {cfg.iterations} episodes per scenario")
+
     if cfg.out_path == "":
         import time
         cfg.out_path = f"out/{int(time.time())}"
+
+    cfg.sim_fps = cfg.sim_steps_per_sec / cfg.sim_steps_per_frame  # TODO 60 or even 120 for dynamic movement?
+    cfg.sim_steps_per_ep = cfg.sim_steps_per_frame * cfg.frames
 
     main(cfg)
