914778c7490cd2f7830f3c4be2ede47a9a0688e1
Andreas Boltres <boltres@ais.uni-bonn.de>
2021-10-26 15:00:46 +0200

    curated YCB objects in OBJECT_INFO

--- ycb_dynamic/CONSTANTS.py
+++ ycb_dynamic/CONSTANTS.py
@@ -106,7 +106,8 @@ ObjectInfo = namedtuple(
 )
 
 # source: http://www.ycbbenchmarks.com/wp-content/uploads/2015/09/object-list-Sheet1.pdf
-# BOP models use millimeter units for some strange reason. sl is completely metric, so scale accordingly.
+# BOP-Compatible YCB-Video models use millimeter units for some strange reason.
+# -> sl is completely metric, so scale accordingly.
 OBJECT_INFO = [
     ObjectInfo(
         "002_master_chef_can",
@@ -280,15 +281,6 @@ OBJECT_INFO = [
         0.001,
     ),
     ObjectInfo(
-        "022_windex_bottle",
-        "ycb_models/022_windex_bottle/google_512k/textured.obj",
-        1.022,
-        FLAG_CONCAVE,
-        0.1,
-        0.5,
-        1.0,
-    ),
-    ObjectInfo(
         "024_bowl",
         "ycbv_models/models_fine/obj_000013.ply",
         0.147,
@@ -316,15 +308,6 @@ OBJECT_INFO = [
         1.0,
     ),
     ObjectInfo(
-        "028_skillet_lid",
-        "ycb_models/028_skillet_lid/google_512k/textured.obj",
-        0.652,
-        FLAG_CONCAVE,
-        0.8,
-        0.1,
-        1.0,
-    ),
-    ObjectInfo(
         "029_plate",
         "ycb_models/029_plate/google_512k/textured.obj",
         0.279,
@@ -332,34 +315,7 @@ OBJECT_INFO = [
         0.6,
         0.3,
         1.0,
-    ),
-    ObjectInfo(
-        "030_fork",
-        "ycb_models/030_fork/google_64k/textured.obj",
-        0.034,
-        FLAG_CONCAVE,
-        0.6,
-        0.3,
-        1.0,
-    ),
-    ObjectInfo(
-        "031_spoon",
-        "ycb_models/031_spoon/google_64k/textured.obj",
-        0.030,
-        FLAG_CONCAVE,
-        0.6,
-        0.3,
-        1.0,
-    ),
-    ObjectInfo(
-        "032_knife",
-        "ycb_models/032_knife/google_64k/textured.obj",
-        0.031,
-        FLAG_CONCAVE,
-        0.6,
-        0.3,
-        1.0,
-    ),
+    ),  # BAD QUALITY
     ObjectInfo(
         "033_spatula",
         "ycb_models/033_spatula/google_512k/textured.obj",
@@ -378,10 +334,9 @@ OBJECT_INFO = [
         0.6,
         0.001,
     ),
-    # ObjectInfo('036_wood_block',        'ycbv_models/models_fine/obj_000016_rotated.ply', 0.729, 0,             0.3, 0.5, 0.001),  # edited
     ObjectInfo(
         "036_wood_block",
-        "ycbv_models/models_fine/obj_000016_rotated.ply",
+        "ycbv_models/models_fine/obj_000016_rotated.ply",  # "ycbv_models/models_fine/obj_000016.ply"
         0.729,
         0,
         0.3,
@@ -396,15 +351,6 @@ OBJECT_INFO = [
         0.1,
         0.5,
         0.001,
-    ),
-    ObjectInfo(
-        "038_padlock",
-        "ycb_models/038_padlock/google_512k/textured.obj",
-        0.208,
-        FLAG_CONCAVE,
-        0.9,
-        0.3,
-        1.0,
     ),
     ObjectInfo(
         "040_large_marker",
@@ -425,10 +371,46 @@ OBJECT_INFO = [
         1.0,
     ),
     ObjectInfo(
+        "043_phillips_screwdriver",
+        "ycb_models/043_phillips_screwdriver/google_512k/textured.obj",
+        0.097,
+        0,  # concave?
+        0.1,
+        0.5,
+        1.0,
+    ),
+    ObjectInfo(
+        "044_flat_screwdriver",
+        "ycb_models/044_flat_screwdriver/google_512k/textured.obj",
+        0.098,
+        0,  # concave?
+        0.1,
+        0.5,
+        1.0,
+    ),
+    ObjectInfo(
+        "048_hammer",
+        "ycb_models/048_hammer/google_512k/textured.obj",
+        0.665,
+        FLAG_CONCAVE,  # concave?
+        0.1,
+        0.5,
+        1.0,
+    ),
+    ObjectInfo(
+        "050_medium_clamp",
+        "ycb_models/050_medium_clamp/google_512k/textured.obj",
+        0.059,
+        FLAG_CONCAVE,  # concave?
+        0.1,
+        0.5,
+        1.0,
+    ),
+    ObjectInfo(
         "051_large_clamp",
         "ycbv_models/models_fine/obj_000019.ply",
         0.125,
-        0,
+        FLAG_CONCAVE,  # concave?
         0.1,
         0.5,
         0.001,
@@ -437,10 +419,64 @@ OBJECT_INFO = [
         "052_extra_large_clamp",
         "ycbv_models/models_fine/obj_000020.ply",
         0.202,
-        0,
+        FLAG_CONCAVE,  # concave?
         0.1,
         0.5,
         0.001,
+    ),
+    ObjectInfo(
+        "053_mini_soccer_ball",
+        "ycb_models/053_mini_soccer_ball/google_512k/textured.obj",
+        0.123,
+        0,
+        0.1,
+        0.3,
+        1.0,
+    ),
+    ObjectInfo(
+        "054_softball",
+        "ycb_models/054_softball/google_512k/textured.obj",
+        0.191,
+        0,
+        0.0,
+        0.7,
+        1.0,
+    ),
+    ObjectInfo(
+        "055_baseball",
+        "ycb_models/055_baseball/google_512k/textured.obj",
+        0.138,
+        0,
+        0.1,
+        0.5,
+        1.0,
+    ),
+    ObjectInfo(
+        "056_tennis_ball",
+        "ycb_models/056_tennis_ball/google_512k/textured.obj",
+        0.058,
+        0,
+        0.0,
+        0.9,
+        1.0,
+    ),
+    ObjectInfo(
+        "057_racquetball",
+        "ycb_models/057_racquetball/google_512k/textured.obj",
+        0.041,
+        0,
+        0.1,
+        0.6,
+        1.0,
+    ),
+    ObjectInfo(
+        "058_golf_ball",
+        "ycb_models/058_golf_ball/google_512k/textured.obj",
+        0.046,
+        0,
+        0.3,
+        0.5,
+        1.0,
     ),
     ObjectInfo(
         "061_foam_brick",
@@ -459,6 +495,132 @@ OBJECT_INFO = [
         0.3,
         0.5,
         1.0
+    ),
+    ObjectInfo(
+        "065-a_cups",
+        "ycb_models/065-a_cups/google_512k/textured.obj",
+        0.013,
+        FLAG_CONCAVE,
+        0.1,
+        0.5,
+        1.0,
+    ),  # BAD QUALITY (interior)
+    ObjectInfo(
+        "065-b_cups",
+        "ycb_models/065-b_cups/google_512k/textured.obj",
+        0.014,
+        FLAG_CONCAVE,
+        0.1,
+        0.5,
+        1.0,
+    ),  # BAD QUALITY (interior)
+    ObjectInfo(
+        "065-c_cups",
+        "ycb_models/065-c_cups/google_512k/textured.obj",
+        0.017,
+        FLAG_CONCAVE,
+        0.1,
+        0.5,
+        1.0,
+    ),  # BAD QUALITY (interior)
+    ObjectInfo(
+        "065-d_cups",
+        "ycb_models/065-d_cups/google_512k/textured.obj",
+        0.019,
+        FLAG_CONCAVE,
+        0.1,
+        0.5,
+        1.0,
+    ),  # BAD QUALITY (interior)
+    ObjectInfo(
+        "065-e_cups",
+        "ycb_models/065-e_cups/google_512k/textured.obj",
+        0.021,
+        FLAG_CONCAVE,
+        0.1,
+        0.5,
+        1.0,
+    ),  # BAD QUALITY (interior)
+    ObjectInfo(
+        "065-f_cups",
+        "ycb_models/065-f_cups/google_512k/textured.obj",
+        0.026,
+        FLAG_CONCAVE,
+        0.1,
+        0.5,
+        1.0,
+    ),  # BAD QUALITY (interior)
+    ObjectInfo(
+        "065-g_cups",
+        "ycb_models/065-g_cups/google_512k/textured.obj",
+        0.028,
+        FLAG_CONCAVE,
+        0.1,
+        0.5,
+        1.0,
+    ),  # BAD QUALITY (interior)
+    ObjectInfo(
+        "065-h_cups",
+        "ycb_models/065-h_cups/google_512k/textured.obj",
+        0.031,
+        FLAG_CONCAVE,
+        0.1,
+        0.5,
+        1.0,
+    ),  # BAD QUALITY (interior)
+    ObjectInfo(
+        "065-i_cups",
+        "ycb_models/065-i_cups/google_512k/textured.obj",
+        0.035,
+        FLAG_CONCAVE,
+        0.1,
+        0.5,
+        1.0,
+    ),  # BAD QUALITY (interior)
+    ObjectInfo(
+        "065-j_cups",
+        "ycb_models/065-j_cups/google_512k/textured.obj",
+        0.038,
+        FLAG_CONCAVE,
+        0.1,
+        0.5,
+        1.0,
+    ),  # BAD QUALITY (interior)
+    ObjectInfo(
+        "070-b_colored_wood_blocks",
+        "ycb_models/070-b_colored_wood_blocks/google_64k/textured.obj",
+        0.011,
+        0,
+        0.3,
+        0.5,
+        1.0,
+    ),
+    ObjectInfo(
+        "071_nine_hole_peg_test",
+        "ycb_models/071_nine_hole_peg_test/google_512k/textured.obj",
+        1.435,
+        0,
+        0.4,
+        0.5,
+        1.0,
+    ),
+    ObjectInfo(
+        "072-a_toy_airplane",
+        "ycb_models/072-a_toy_airplane/google_512k/textured.obj",
+        0.570,
+        FLAG_CONCAVE,
+        0.3,
+        0.5,
+        1.0,
+    ),
+    ObjectInfo(
+        "077_rubiks_cube",
+        "ycb_models/077_rubiks_cube/google_512k/textured.obj",
+        0.094,
+        0,
+        0.1,
+        0.4,
+        1.0,
     ),
     ObjectInfo(
         "table",
@@ -514,13 +676,17 @@ FRUIT_OBJS = [str(i).zfill(3) for i in range(11, 19)]
 CLEANING_OBJS = [str(i).zfill(3) for i in range(19, 22)]
 KITCHEN_OBJS = [str(i).zfill(3) for i in range(22, 35)]
 DICE_OBJS = [str(i).zfill(3) for i in [5, 7, 8, 9]]
+YCBV_OBJS = [str(i).zfill(3) for i in [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 19, 21, 24, 25, 35, 36, 37, 40, 51, 52, 61]]
 
-YCBV_OBJECTS = [obj for obj in OBJECT_INFO if obj.name[0].isdigit()]
+YCB_OBJECTS = [obj for obj in OBJECT_INFO if obj.name[0].isdigit()]
 BOWL = [obj for obj in OBJECT_INFO if obj.name == "red_bowl"]
 TABLE = [obj for obj in OBJECT_INFO if obj.name == "table"]
 BOWLING_BALL = [obj for obj in OBJECT_INFO if obj.name == "bowling_ball"]
 BEACH_BALL = [obj for obj in OBJECT_INFO if obj.name == "beach_ball"]
 WOOD_BLOCK = [obj for obj in OBJECT_INFO if obj.name == "036_wood_block"]
+YCBV_OBJECTS = [  # Only considering the YCB-Video subset of objects
+    obj for obj in OBJECT_INFO if obj.name.split("_")[0] in YCBV_OBJS
+]
 STACK_OBJECTS = [  # Only considering a subset of 'stackable' objects
     obj for obj in OBJECT_INFO if obj.name.split("_")[0] in FLAT_OBJS
 ]
