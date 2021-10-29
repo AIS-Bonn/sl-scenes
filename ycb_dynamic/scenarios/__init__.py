49a25782486c5d7f518d1770443134d979c5ad71
Andreas Boltres <boltres@ais.uni-bonn.de>
2021-10-29 11:39:59 +0200

    removed collision scenario

--- ycb_dynamic/scenarios/__init__.py
+++ ycb_dynamic/scenarios/__init__.py
@@ -1,6 +1,5 @@
 from .billiards import BillardsScenario
 from .bowling import BowlingScenario
-from .collision import CollisionScenario
 from .dice_roll import DiceRollScenario
 from .stack import StackScenario
 from .throw import ThrowScenario
